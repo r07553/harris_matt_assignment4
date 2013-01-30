1ac1a0de5cd7865a9bc4f8d4143d14d943a586b9
Matt Harris <mattharris@fullsail.edu>
2013-01-30 15:24:06 -0500

    Added String Problem 1
    Phone Number Problem

--- Project_4/harris_matt_assignment4.js
+++ Project_4/harris_matt_assignment4.js
@@ -5,3 +5,21 @@
 // mattharris@fullsail.edu
 // Project 4 
 // Code Library
+
+// String Functions
+
+//Problem 1 Phone Number String
+
+var phoneNumber = function (number) {
+        var thisPhone = number;
+        var phoneStyle = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+        if (phoneStyle.test(thisPhone)) { 
+        var correctStyle = thisPhone.replace(phoneStyle, "($1) $2-$3");
+            return true; 
+        } else {
+            return false; 
+       }
+};
+
+var thisNumber = phoneNumber("231-912-0745");
+console.log(thisNumber);
